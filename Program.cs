e50e45e514ca762a9c3665d53b2f3b12aa76fd42
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-24 22:31:25 -0300

    feitinhoo eu ygor sou muito fofinho

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double tamanhoBytes, velocidadeMbps, tempoMin;
+
+Console.WriteLine("--- Tempo de Download ---\n");
+
+Console.Write("Tamanho do arquivo em MB........: ");
+tamanhoBytes = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Velocidade da conexão em Mbps...: ");
+velocidadeMbps = Convert.ToDouble(Console.ReadLine());
+
+tempoMin = tamanhoBytes * 8 / velocidadeMbps / 60;
+
+Console.WriteLine($"\nTempo estimado de download: {tempoMin:N1} minutos");
